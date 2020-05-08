8ed20b2bf218c96e663a8f8e168f7ccaa9f13d0c
ThiagoKichiro <kichiro.88@gmail.com>
2020-05-07 22:24:24 -0300

    Controller de Empresas

---
+++ src/main/java/com/trk/inteligente/api/controllers/EmpresaController.java
@@ -0,0 +1,66 @@
+package com.trk.inteligente.api.controllers;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trk.inteligente.api.dtos.EmpresaDto;
+import com.trk.inteligente.api.entities.Empresa;
+import com.trk.inteligente.api.response.Response;
+import com.trk.inteligente.api.services.EmpresaService;
+
+@RestController
+@RequestMapping("/api/empresas")
+@CrossOrigin(origins = "*")
+public class EmpresaController {
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaController.class);
+
+	@Autowired
+	private EmpresaService empresaService;
+
+	/**
+	 * Retorna uma empresa pelo CNPJ
+	 * 
+	 * @param cnpj
+	 * @return ResponseEntity<Response<EmpresaDto>>
+	 */
+	@GetMapping(value="cnpj/{cnpj}")
+	public ResponseEntity<Response<EmpresaDto>> buscarPorCnpj(@PathVariable("cnpj") String cnpj){
+		log.info("Buscando empresa por CNPJ: {}", cnpj);
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		Optional<Empresa> empresa = empresaService.buscarPorCnpj(cnpj);
+		
+		if(!empresa.isPresent()) {
+			log.info("Empresa não encontrada para o CNPJ: " + cnpj);
+			response.getErrors().add("Empresa não encontrada para o CNPJ " + cnpj);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(this.converterEmpresaSto(empresa.get()));
+		return ResponseEntity.ok(response);
+	}
+	
+	/**
+	 * Popula um DTO com os dados de uma empresa
+	 * 
+	 * @param empresa
+	 * @return EmpresaDto
+	 */
+	private EmpresaDto converterEmpresaSto(Empresa empresa) {
+		EmpresaDto empresaDto = new EmpresaDto();
+		empresaDto.setId(empresa.getId());
+		empresaDto.setCnpj(empresa.getCnpj());
+		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		return empresaDto;
+	}
+
+}
